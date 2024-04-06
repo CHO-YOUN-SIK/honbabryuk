dffe8f1368093de2620127cfa131a395a49bd918
CHO-YOUN-SIK <dbstlr1027@gmail.com>
2024-04-06 17:55:26 +0900

    DD

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "혼밥력",
+  description: "",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
