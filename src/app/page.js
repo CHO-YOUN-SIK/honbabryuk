16f8e0c76c8d3ad1460372580a6f6b36838848d7
CHO-YOUN-SIK <dbstlr1027@gmail.com>
2024-04-06 17:46:04 +0900

    structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,59 +1,7 @@
 import styles from "./page.module.css";
+import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.grid}>
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            혼밥력
-          </h2>
-          <p>메인 페이지로 가기</p>
-        </a>
-
-        <a
-          href="src\pages\main.js"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            글쓰기
-          </h2>
-          <p>자신의 혼밥력을 자랑해보자!</p>
-        </a>
-
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href=""
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  return (<></>
   );
 }
