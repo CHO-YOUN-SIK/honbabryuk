8d27592fae6ae938d3bef8d06a13d102173ac883
조윤식 <dbstlr1027@gmail.com>
2024-04-09 20:46:19 +0900

    css 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "혼밥력",
   description: "",
+  icons: {
+    icon: "/bookmark.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
